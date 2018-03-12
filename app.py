665003f32da030616a2720985a9f3f3dfecc6b9a
sushant10 <sushdude34@gmail.com>
2018-03-11 22:28:22 -0700

    more testing

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -22,5 +22,16 @@ tasks = [
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+ 
 if __name__ == '__main__':
     app.run(debug=True)
