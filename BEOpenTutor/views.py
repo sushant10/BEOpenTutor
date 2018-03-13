1b81c0f39aa699b79b045efa43ab5c639c19183e
sushant10 <sushdude34@gmail.com>
2018-03-12 19:08:33 -0700

    More testing with mLab and mongoDB

--- BEOpenTutor/views.py
+++ BEOpenTutor/views.py
@@ -1,11 +1,17 @@
 from flask import (
-    Blueprint, render_template, session, g, flash, request, redirect, url_for,
-    current_app, jsonify
+	Blueprint, render_template, session, g, flash, request, redirect, url_for,
+	current_app, jsonify
 )
 
-from run import app
+from run import app, mongo
 
 @app.route('/', methods=['GET'])
-def get_tasks():
-    return 200
-    
+def main_test():
+	u = mongo.db.Users
+	output = []
+
+	for q in u.find():
+		output.append({'email' : q['username'], 'major' : q['major']})
+
+	return jsonify({'result' : output})
+
