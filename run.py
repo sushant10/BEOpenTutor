2c6b5f518565be699628f265f7196ea63f808d85
sushant10 <sushdude34@gmail.com>
2018-03-23 13:48:45 -0400

    added logging to server

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import logging
 
 app = Flask(__name__)
 
@@ -10,4 +11,7 @@ mongo = PyMongo(app)
 from BEOpenTutor.views import *
 
 if __name__ == '__main__':
+	logger = logging.getLogger('werkzeug')
+	handler = logging.FileHandler('access.log')
+	logger.addHandler(handler)
 	app.run(host='0.0.0.0', port=80)
